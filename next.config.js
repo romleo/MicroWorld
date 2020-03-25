18aab0b48ef25cfd08f490b17ca80576fb6c588b
Roman <romandybinetc@gmail.com>
2020-03-25 14:52:51 +0200

    update element3

--- next.config.js
+++ next.config.js
@@ -1,3 +1,16 @@
+const withPlugins = require('next-compose-plugins');
+const withTM = require('next-transpile-modules')(['@edooking/ui']);
+const withOptimizedImages = require('next-optimized-images');
+const withFonts = require('next-fonts');
+
+const nextJsConfig = {
+  target: 'serverless',
+};
+
+module.exports = withPlugins(
+  [withTM, withOptimizedImages, withFonts],
+  nextJsConfig,
+);
 const path = require('path')
 
 module.exports = {
